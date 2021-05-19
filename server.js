37a9a15a573701def416d68bb0f172f9c317a6a6
Ankush <ankush@anilabsinc.com>
2021-05-19 13:49:18 +0530

    change

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// // set the port of our application
-// // process.env.PORT lets the port be set by Heroku
-// var port = process.env.PORT || 8080;
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-// // set the view engine to ejs
-// app.set('view engine', 'ejs');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-// // make express look in the public directory for assets (css/js/img)
-// app.use(express.static(__dirname + '/public'));
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-// // set the home page route
-// app.get('/', function(req, res) {
+// set the home page route
+app.get('/', function(req, res) {
 
-// 	// ejs render automatically looks in the views folder
-// 	res.render('index');
-// });
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
 
-// app.listen(port, function() {
-// 	console.log('Our app is running on http://localhost:' + port);
-// });
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
